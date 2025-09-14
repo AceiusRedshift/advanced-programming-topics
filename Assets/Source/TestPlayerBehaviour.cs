0b82ea96a190ce144805c79ab69ad7f1946ea006
Aceius E. <io@acei.us>
2025-09-13 18:13:00 -0700

    Working jumping

--- Assets/Source/TestPlayerBehaviour.cs
+++ Assets/Source/TestPlayerBehaviour.cs
@@ -1,31 +1,35 @@
-using System;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TestPlayerBehaviour : MonoBehaviour
 {
     private const float kMoveForce = 25;
-    private const float kJumpForce = 50;
+    private const float kJumpForce = 500;
     private const float kGroundedRadius = .2f;
 
     private InputAction movementAction;
     private InputAction jumpAction;
 
     private Rigidbody2D rigidBody;
+    private Collider2D hitbox;
+    
+    private int groundLayerMask;
     private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         movementAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
 
         rigidBody = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<Collider2D>();
+        
+        groundLayerMask = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector2 wishVector = movementAction.ReadValue<Vector2>();
 
@@ -39,19 +43,7 @@ public class TestPlayerBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        bool wasGrounded = isGrounded;
-        isGrounded = false;
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, kGroundedRadius);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject != gameObject)
-            {
-                isGrounded = true;
-                // if (!wasGrounded)
-                //     OnLandEvent.Invoke();
-            }
-        }
+        isGrounded = Physics2D.IsTouchingLayers(hitbox, groundLayerMask);
     }
 
     private void OnGUI()
